22838d63d354d78ae4ff1ec217c8261f6d940a63
aabubakir-dev <abubakir.work@gmail.com>
2025-11-28 21:51:30 +0500

    Простой калькулятор

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('test')
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+operation = input("Введите операцию (+, -, *, /): ")
+
+if operation == '+':
+    print(f"Результат {num1 + num2}")
+
+elif operation == '-':
+    print(f"Result = {num1 - num2}")
+
+elif operation == "*":
+    print(f"Result = {num1 * num2}")
+
+else:
+    print(f"Result = {num1 / num2}")
